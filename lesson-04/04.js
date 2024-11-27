380190c3129d9a7f14b4516b9c0ecdc2ff1a662f
Nickrosenberg <beltiukovrosenberg@gmail.com>
2024-11-27 18:41:32 +0300

    task-4

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,20 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+    let doubledString = ''; // Инициализируем пустую строку для хранения результата
+
+    // Проходим по каждому символу в строке
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i]; // Получаем текущий символ
+        doubledString += char.repeat(2); // Удваиваем символ и добавляем к результату
+    }
+
+    return doubledString; // Возвращаем новую удвоенную строку
+}
+
+// Примеры использования
+console.log(doubleEachCharacter('hello')); // 'hheelllloo'
+console.log(doubleEachCharacter('world!')); // 'wwoorrlldd!!'
+console.log(doubleEachCharacter('123')); // '112233'
+console.log(doubleEachCharacter('')); // ''
