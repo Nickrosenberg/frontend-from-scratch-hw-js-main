ce2cd33beb0394767727454c6054d7d09d111935
Nickrosenberg <beltiukovrosenberg@gmail.com>
2024-12-01 18:17:06 +0300

    task-2

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -36,3 +36,62 @@ const gallery = {
 }
 
 function updateGallery() {}
+
+
+/*
+Напишите функцию `updateGallery`, которая будет обновлять информацию о произведениях искусства в виртуальной галерее. Функция должна принимать три параметра:
+
+- объект галереи
+- название произведения (ключ)
+- новое значение
+
+Если произведение с таким названием уже есть в галерее, его значение должно быть обновлено. Если произведения нет, оно должно быть добавлено в объект галерею.
+
+Пример использования функции:
+
+const gallery = {
+  'Mona Lisa': 'Leonardo da Vinci',
+  'Starry Night': 'Vincent van Gogh',
+  'The Scream': 'Edvard Munch'
+}
+
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
+
+console.log(gallery)
+
+Ожидаемый вывод:
+{
+  'Mona Lisa': 'Leonardo da Vinci, 1503-1506',
+  'Starry Night': 'Vincent van Gogh',
+  'The Scream': 'Edvard Munch',
+  'The Persistence of Memory': 'Salvador Dali'
+}
+*/
+
+
+
+onst gallery = {
+  'Mona Lisa': 'Leonardo da Vinci',
+  'Starry Night': 'Vincent van Gogh',
+  'The Scream': 'Edvard Munch'
+}
+
+function updateGallery(gallery, artwork, newValue) {
+  // Проверяем, существует ли произведение в галерее
+  if (gallery.hasOwnProperty(artwork)) {
+      // Если существует, обновляем значение
+      gallery[artwork] = newValue;
+      console.log(`Обновлено: ${artwork} - ${newValue}`);
+  } else {
+      // Если не существует, добавляем новое произведение
+      gallery[artwork] = newValue;
+      console.log(`Добавлено: ${artwork} - ${newValue}`);
+  }
+}
+
+// Примеры использования функции
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506');
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali');
+
+console.log(gallery);
