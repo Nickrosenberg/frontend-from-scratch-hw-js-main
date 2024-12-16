a2d3bce6a1fc48899cd8a6814204b70b42996a45
Nickrosenberg <beltiukovrosenberg@gmail.com>
2024-12-16 12:50:20 +0300

    task-1-m

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -6,28 +6,9 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
-
 function capitalizeWords(str) {
-  // Разбиваем строку на массив слов
-  const words = str.split(' ');
-  
-  // Создаем массив для хранения результата
-  const capitalizedWords = [];
-  
-  // Проходим по каждому слову и делаем первую букву заглавной
-  for (let i = 0; i < words.length; i++) {
-      // Делаем первую букву заглавной, а остальные малой
-      const capitalizedWord = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
-      // Добавляем измененное слово в массив
-      capitalizedWords.push(capitalizedWord);
-  }
-  
-  // Соединяем слова обратно в строку и возвращаем результат
-  return capitalizedWords.join(' ');
+  return str
+      .split(' ') // Разделяем строку на слова
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()) // Мапим каждое слово
+      .join(' '); // Соединяем слова обратно в строку
 }
-
-// Пример использования
-const inputStr = "hello world from javascript";
-const result = capitalizeWords(inputStr);
-console.log(result); // Вывод: "Hello World From Javascript"
