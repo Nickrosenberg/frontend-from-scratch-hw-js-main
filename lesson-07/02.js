e0089f060e4f9e27f27ef01f71d323abe7a6b92d
Nickrosenberg <beltiukovrosenberg@gmail.com>
2024-12-16 12:55:15 +0300

    task-2

--- lesson-07/02.js
+++ lesson-07/02.js
@@ -3,12 +3,29 @@
  * Если строка является числом, функция должна возвращать true, в противном случае - false.
  */
 
+// function isNumeric(str) {
+//   // your code
+// }
+
+// // console.log(isNumeric("123")) // Ожидаемый результат: true
+// // console.log(isNumeric("12.3")) // Ожидаемый результат: true
+// // console.log(isNumeric("123abc")) // Ожидаемый результат: false
+// // console.log(isNumeric("abc")) // Ожидаемый результат: false
+// // console.log(isNumeric(" ")) // Ожидаемый результат: false
+
+
 function isNumeric(str) {
-  // your code
+  // Удаляем пробелы в начале и конце строки
+  str = str.trim();
+
+  // Проверяем, является ли str числом
+  return !isNaN(str) && !isNaN(parseFloat(str)) && str !== "";
 }
 
-// console.log(isNumeric("123")) // Ожидаемый результат: true
-// console.log(isNumeric("12.3")) // Ожидаемый результат: true
-// console.log(isNumeric("123abc")) // Ожидаемый результат: false
-// console.log(isNumeric("abc")) // Ожидаемый результат: false
-// console.log(isNumeric(" ")) // Ожидаемый результат: false
+// Примеры использования
+console.log(isNumeric("123"));      // Ожидаемый результат: true
+console.log(isNumeric("12.3"));     // Ожидаемый результат: true
+console.log(isNumeric("123abc"));   // Ожидаемый результат: false
+console.log(isNumeric("abc"));      // Ожидаемый результат: false
+console.log(isNumeric(" "));        // Ожидаемый результат: false
+
